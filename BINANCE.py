43f700e220e9bf0af42a4a845c4008a84b4bfda8
jangho125 <102157595+jangho125@users.noreply.github.com>
2022-04-22 18:55:30 -0400

    Update BINANCE.py

--- BINANCE.py
+++ BINANCE.py
@@ -1,67 +1,217 @@
 import ccxt
+import larrybybit
 import time
 import datetime
-import pandas as pd
-import larry
-import math
+import bestk
 
 
-api_key = "2xc04G17wowEe1MWORfWDE4YSztgs75gTha3RVsNaYzCrMDikX7AzsoTHPB1VPZT"
-secret = "H5Q8BgllV4uqBPfQqLWGv4O6rI6JZWIrWZ4b3VX5oLmJKk4AgIrAv4V7zwfmKHVX"
+
+api_key = "gjNFGc1OwP5s8LOaZR"
+secret = "h9IwB4c07TOh8dT9hMB1nDhOSJH3ntGuvvtk"
+
 
 # binance 객체 생성
-binance = ccxt.binance(config={
+bybit = ccxt.bybit(config={
     'apiKey' : api_key,
     'secret' : secret,
     'enableratelimit' : True,
     'options' : {'defaultType' : 'future'}
 })
 
-symbol = "BTC/USDT"
-long_target, short_target = larry.cal_target(binance, symbol)
 
-# 잔고
-balance = binance.fetch_balance()
-usdt = balance['total']['USDT']
+
+
+symbol = "BTC/USDT"
+timeframe = '4h'
+fees = 0.0007
+leverage = 3
+
+
+balance = bybit.fetch_balance()
+usdt_balance = balance['total']['USDT']
+buy_average = larrybybit.get_avg_price(bybit, symbol)
+long_crr = bestk.get_best_long_crr(symbol, fees)
+short_crr = bestk.get_best_short_crr(symbol, fees)
+highest_value = larrybybit.highest_value(bybit, symbol)
+lowest_value = larrybybit.lowest_value(bybit, symbol)
+
+print(usdt_balance)
 
 position = {
     "type": None,
     "amount": 0
 }
-op_mode = False
 
 
 while True:
-    # time
-    now = datetime.datetime.now()
     
-    #포지션 종료
-    if now.hour ==8 and now.minute == 50 and (0<= now.second < 10):
-       if op_mode and position['type'] is not None:
-           larry.exit_position(binance, symbol, position)
-           op_mode = False
-        
-    
-    # 목표가 갱신
-    if now.hour ==9 and now.minute == 0 and (20<= now.second < 30):
-        long_target, short_target = larry.cal_target(binance, symbol)
-        balance = binance.fetch_balance()
-        usdt = balance['total']['USDT']
-        op_mode = True
-        time.sleep(10)
-        
-    # 현재가, 구매가능 수량 (10%)
-    btc = binance.fetch_ticker(symbol=symbol)
-    cur_price = btc['last']
-    amount = larry.cal_amount(usdt, cur_price)
-    
-    if op_mode and position['type'] is None:
-        larry.enter_position(binance, symbol, cur_price, long_target, short_target, amount, position)
-        
-   
+    start_time = larrybybit.get_start_time(bybit, symbol, timeframe) 
+    now = datetime.datetime.now() 
+    end_time = start_time + datetime.timedelta(minutes=240) - datetime.timedelta(seconds=20) # 매매 시작
     time.sleep(1)
-
-
+    print(start_time)
+    print(now)
+    print(end_time)    
+        
+    if start_time < now < end_time:
+        long_target, short_target = larrybybit.cal_target(bybit, symbol)
+        
+        
+        
+        i = 0
+        while i < 4:
+            now = datetime.datetime.now()
+            btc = bybit.fetch_ticker(symbol)
+            cur_price = btc['last']
+            amount = larrybybit.cal_amount(usdt_balance, cur_price, leverage)
+            time.sleep(1)
+            
+           
+            print("script is running")
+            
+            
+            
+            # 1차 매수
+            
+            if i == 0 and (long_target -100) <= cur_price < (long_target + 100) and long_crr > 1:
+                position['type'] = 'long'
+                position['amount'] = amount
+                bybit.create_market_buy_order(symbol, amount * 0.25)
+                time.sleep(1)
+                buy_average = larrybybit.get_avg_price(bybit, symbol)
+                i += 1
+                print("1차 LONG 매수 성공")
+            elif i == 0 and (short_target - 100) <= cur_price < (short_target + 100) and short_crr > 1:
+                position['type'] = 'short'
+                position['amount'] = amount
+                bybit.create_market_sell_order(symbol, amount * 0.25)
+                time.sleep(1)
+                buy_average = larrybybit.get_avg_price(bybit, symbol)
+                i += 1
+                print("1차 SHORT 매수 성공")
+                
+            
+            # 2차 매수    
+            
+            if i == 1 and cur_price < float(buy_average) * 0.94:
+                position['type'] = 'long'
+                position['amount'] = amount
+                bybit.create_market_buy_order(symbol, amount * 0.25)
+                time.sleep(1)
+                buy_average = larrybybit.get_avg_price(bybit, symbol)
+                i += 1
+                print("2차 LONG 매수 성공")
+            elif i == 1 and cur_price > float(buy_average) * 1.06:
+                position['type'] = 'short'
+                position['amount'] = amount
+                bybit.create_market_sell_order(symbol, amount * 0.25)
+                time.sleep(1)
+                buy_average = larrybybit.get_avg_price(bybit, symbol)
+                i += 1
+                print("2차 SHORT 매수 성공")
+                
+                
+            # 3차 매수
+            
+            if i == 2 and cur_price < float(buy_average) * 0.94:
+                position['type'] = 'long'
+                position['amount'] = amount
+                bybit.create_market_buy_order(symbol, amount * 0.5)
+                time.sleep(1)
+                buy_average = larrybybit.get_avg_price(bybit, symbol)
+                i += 1
+                print("3차 LONG 매수 성공")
+            elif i == 2 and cur_price > float(buy_average) * 1.06:
+                position['type'] = 'short'
+                position['amount'] = amount
+                bybit.create_market_sell_order(symbol, amount * 0.5)
+                time.sleep(1)
+                buy_average = larrybybit.get_avg_price(bybit, symbol)
+                i += 1  
+                print("3차 SHORT 매수 성공")
+                
+            
+            # 매도 조건
+            
+            if i == 3 and cur_price < float(buy_average) * 0.95:
+                if position['type'] == 'long':
+                    bybit.create_market_sell_order(symbol, amount)
+                    time.sleep(1)
+                    position['type'] == None
+                    print("3차 LONG 매수 후 5% 손절")
+                
+            elif i == 3 and cur_price > float(buy_average) * 1.05:
+                if position['type'] == 'short':
+                    bybit.create_market_buy_order(symbol, amount)
+                    time.sleep(1)
+                    position['type'] == None
+                    print("3차 SHORT매수 후 5% 손절")
+            
+            if cur_price > float(buy_average) * 1.06:
+                if i == 0:
+                    amount = position['amount'] * 0.25
+                elif i == 1:
+                    amount = position['amount'] * 0.5
+                elif i == 2:
+                    amount = position['amount'] 
+                elif i == 3:
+                    amount = position['amount'] 
+                    
+                price = highest_value * 0.97
+                if position['type'] == 'long':
+                    bybit.create_limit_sell_order(symbol, amount, price)
+                    time.sleep(1)
+                    position['type'] == None
+                    print("LONG 익절")
+                    
+            elif cur_price < float(buy_average) * 0.94:
+                if i == 0:
+                    amount = position['amount'] * 0.25
+                elif i == 1:
+                    amount = position['amount'] * 0.5
+                elif i == 2:
+                    amount = position['amount'] 
+                elif i == 3:
+                    amount = position['amount']    
+                
+                price = lowest_value * 1.03
+                if position['type'] == 'short':
+                    bybit.create_limit_buy_order(symbol, amount, price)
+                    time.sleep(1)
+                    position['type'] == None
+                    print("SHORT 익절")
+            
+            # 매매 종료
+            
+            if now > end_time:
+                if i == 0:
+                    amount = position['amount'] * 0.25
+                elif i == 1:
+                    amount = position['amount'] * 0.5
+                elif i == 2:
+                    amount = position['amount'] 
+                elif i == 3:
+                    amount = position['amount']
+                    
+                if position['type'] == 'long':
+                    bybit.create_market_sell_order(symbol, amount)
+                    time.sleep(1)
+                    position['type'] == None
+                    print("4시간 후 LONG 매도")
+                    
+                    break 
+                
+                elif position['type'] == 'short':
+                    bybit.create_market_buy_order(symbol, amount)
+                    time.sleep(1)
+                    position['type'] == None
+                    print("4시간 후 SHORT 매도")
+                    
+                    break
+            
+    
+   
+        
 
    
     
